906a5e111c4de4f7cd99554ba51e79ffb9921b40
Sandro Pasquali <sandro.pasquali@ehealthtechnologies.com>
2015-10-20 12:56:27 -0400

    added validation logic and optional validators, added tests

--- test/spec/suite.js
+++ test/spec/suite.js
@@ -1,14 +1,13 @@
 "use strict";
 
 var path = require('path');
-var schemap = require('../../lib');
+var schemap = require('../../lib')();
 
 module.exports = function(test, Promise) {
 
     var testkey = 'testkey';
     var testSchema = require(path.resolve(__dirname, '../assets/test_schema.json'));
-    var schemaSet = schemap.set(testkey, testSchema);
-    var schemaGet = schemap.get(testkey);
+
     var goodJSON = {
         firstName: 'Jack',
         lastName: 'Spratt'
@@ -19,13 +18,62 @@ module.exports = function(test, Promise) {
         age: "eighteeen" // Error! Schema expects an Integer
     };
 
-    test.ok(schemaSet, 'schema was successfully #set');
+    // Test against all validators
+    //
+    ['ajv','jsen','isMyJSONValid'].forEach(function(validator) {
 
-    test.equal(schemaSet, schemaGet, '#get got the same schema that was #set');
+        schemap.use(validator)
 
-    test.equal(schemap.validate(testkey, goodJSON), true, 'valid JSON is passing');
+        var schemaSet = schemap.set(testkey, testSchema);
+        var schemaGet = schemap.get(testkey);
 
-    test.notEqual(schemap.validate(testkey, badJSON), true, 'invalid JSON is failing');
+        test.ok(schemaSet, 'schema was successfully #set using -> ' + validator);
+
+        test.equal(schemaSet, schemaGet, '#get got the same schema that was #set using -> ' + validator);
+
+        test.equal(schemap.validate(testkey, goodJSON), true, 'valid JSON is passing using -> ' + validator);
+
+        test.notEqual(schemap.validate(testkey, badJSON), true, 'invalid JSON is failing using -> ' + validator);
+    })
+
+    // Test API call signature error handling
+    //
+
+    // #set
+    //
+    test.throws(function() {
+        schemap.set('foo', 2)
+    }, 'Schemap throws if non-object schema sent to #set');
+
+    test.throws(function() {
+        schemap.set(1, {})
+    }, 'Schemap throws if non-string key sent to #set');
+
+    // #get
+    //
+    test.throws(function() {
+        schemap.get('foo')
+    }, 'Schemap throws if non-existent key sent to #get');
+
+    // #use
+    //
+    test.equal(schemap.use('foo'), false, 'Schemap returns false on unrecognized validator name');
+
+    test.equal(schemap.use('ajv'), true, 'Schemap returns true on recognized validator name');
+
+    // #validate
+    //
+    test.throws(function() {
+        schemap.validate('foo', 2)
+    }, 'Schemap throws if non-object schema sent to #validate');
+
+    test.throws(function() {
+        schemap.validate(1, {})
+    }, 'Schemap throws if non-string key sent to #validate');
+
+    test.throws(function() {
+        schemap.validate('foo', {})
+    }, 'Schemap throws if non-existent key sent to #validate');
 
     return Promise.resolve();
 };
